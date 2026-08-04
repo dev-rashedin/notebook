b1b9a65d2d6850c30a41eb956e2820b848240c07
dev-rashedin <rashedinislam.06@gmail.com>
2026-08-04 15:19:16 +0600

    unnecessary files deleted

--- src/app/(pages)/(navbar)/blog/page.tsx
+++
@@ -1,9 +0,0 @@
-const ResourcesPage = () => {
-  return (
-    <main className="min-h-[80vh] flex-center">
-      <h3 className="text-lg lg:text-2xl font-semibold">No blog has been published yet</h3>
-    </main>
-  );
-};
-
-export default ResourcesPage;
