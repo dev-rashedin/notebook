14d36c76e3da19b8990c3c4031909046902278bd
dev-rashedin <rashedinislam.06@gmail.com>
2026-08-11 00:24:18 +0600

    pages layout component created

---
+++ src/components/PagesLayout.tsx
@@ -0,0 +1,28 @@
+import { ReactNode } from 'react';
+import LeftSidebar from '@/components/LeftSidebar';
+import { getContentLinks } from '@/lib';
+import MotionWrapper from '@/components/MotionWrapper';
+import DrawerContainer from '@/components/DrawerContainer';
+import { getNestedLinks } from '@/lib/getLinks';
+
+export default function PagesLayout({ children, slug }: { children: ReactNode, slug: string }) {
+  const links = getContentLinks(slug);
+  
+  
+  const nestedLinks = getNestedLinks(slug);
+
+  return (
+    <main className="lg:flex">
+      <div className="sidebar">
+        <LeftSidebar links={links} type={slug} nestedLinks={nestedLinks} />
+      </div>
+
+      {/* content area */}
+      <div className="docs-content relative">
+        <DrawerContainer links={links} type={slug} nestedLinks={nestedLinks}>
+          <MotionWrapper type="docs">{children}</MotionWrapper>
+        </DrawerContainer>
+      </div>
+    </main>
+  );
+}
