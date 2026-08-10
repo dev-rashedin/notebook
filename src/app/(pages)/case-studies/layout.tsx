dbf93cc528af3949c9d649c1baf7868e7e8abeb8
dev-rashedin <rashedinislam.06@gmail.com>
2026-08-11 00:26:12 +0600

    more docs added for aivinn

--- src/app/(pages)/case-studies/layout.tsx
+++ src/app/(pages)/case-studies/layout.tsx
@@ -4,25 +4,9 @@ import { getContentLinks } from '@/lib';
 import MotionWrapper from '@/components/MotionWrapper';
 import DrawerContainer from '@/components/DrawerContainer';
 import { getNestedLinks } from '@/lib/getLinks';
+import PagesLayout from '../layout';
 
 export default function CaseStudiesLayout({ children }: { children: ReactNode }) {
-  const links = getContentLinks('case-studies');
-  
-  
-  const nestedLinks = getNestedLinks('case-studies');
-
-  return (
-    <main className="lg:flex">
-      <div className="sidebar">
-        <LeftSidebar links={links} type="case-studies" nestedLinks={nestedLinks} />
-      </div>
-
-      {/* content area */}
-      <div className="docs-content relative">
-        <DrawerContainer links={links} type="case-studies" nestedLinks={nestedLinks}>
-          <MotionWrapper type="docs">{children}</MotionWrapper>
-        </DrawerContainer>
-      </div>
-    </main>
+  return (<PagesLayout slug="case-studies">{children}</PagesLayout>
   );
 }
